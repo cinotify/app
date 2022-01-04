8549a7c25fa8a49fca658e983e78fa4fb5cbae77
Jesse Shawl <jesse@jshawl.com>
2022-01-04 14:25:12 -0600

    Improve code coverage (#1)

---
+++ spec/controllers/api/api_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Api::ApiController, type: :controller do
+  it 'POSTs /notify with params' do
+    expect_any_instance_of(Notification).to receive(:deliver)
+    expect{
+      post :notify, params:
+        {
+          api: {
+            to: 'example@example.com',
+            subject: 'hello',
+            body: 'goodbye'
+          }
+        }
+    }.to change{Notification.count}.by(1)
+  end
+end
