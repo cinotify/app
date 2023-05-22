e3c2a587e69fd213ea70c681bad79ae9d2495b52
Jesse Shawl <jesse@jesse.sh>
2023-05-21 20:21:55 -0500

    Discord webhook (#7)
    * add discord webhook url
    
    * add discord notification
    
    * distinguish signup/activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :notifications
+
+  after_create :discord_webhook
+
+  private
+  def discord_webhook
+    emoji = ["🥳","🎉","🙌","🎊"].sample
+    action = encrypted_password? ? "signup" : "activation"
+    Discord.post_message("#{emoji} new #{action}! #{email}")
+  end
 end
--- spec/controllers/api/api_controller_spec.rb
+++ spec/controllers/api/api_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::ApiController, type: :controller do
+  before do
+    stub_request(:post, /https:\/\/discord.com\/(.*)/).
+      to_return(status: 204, body: "", headers: {})
+  end
   it 'POSTs /notify with params' do
     expect_any_instance_of(Notification).to receive(:deliver)
     expect{
