c5abb6e1fc2d4cb6b498524073fd76c9df21aae2
Jesse Shawl <jesse@jesse.sh>
2023-06-11 13:23:11 -0500

    allow multiple recipients (#10)

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,17 +1,36 @@
+# frozen_string_literal: true
+
 class Notification < ApplicationRecord
   belongs_to :user
   after_create :deliver
   scope :recent, -> { order('created_at DESC').limit(10) }
   private
+
   def deliver
     content = SendGrid::Content.new(type: 'text/plain', value: body)
     mail = SendGrid::Mail.new(
-      SendGrid::Email.new(email: 'app@cinotify.cc'), 
-      subject, 
-      SendGrid::Email.new(email: to), 
+      SendGrid::Email.new(email: 'app@cinotify.cc'),
+      subject,
+      SendGrid::Email.new(email: to),
       content
     )
     sg = SendGrid::API.new(api_key: Rails.application.credentials.sendgrid[:api_key])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+
+  def self.from_params(params)
+    emails = params[:to].split(',')
+    notifications = emails.map do |email|
+      user = User.find_or_initialize_by(email:)
+      user.save(validate: false)
+      user.notifications.create!(
+        { to: email.squish }.merge(params.permit(:subject, :body))
+      )
+    end
+    if notifications.count == 1
+      notifications[0]
+    else
+      notifications
+    end
   end
 end
