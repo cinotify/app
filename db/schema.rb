988bf9870ae5e76577b51deb219effbe23e6930e
Jesse Shawl <jesse@jesse.sh>
2023-10-28 12:12:35 -0500

    Send attachments (#12)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_15_145356) do
+ActiveRecord::Schema.define(version: 2023_10_28_141221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2023_04_15_145356) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.json "attachments", default: []
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
--- spec/controllers/api/api_controller_spec.rb
+++ spec/controllers/api/api_controller_spec.rb
@@ -56,4 +56,25 @@ RSpec.describe Api::ApiController, type: :controller do
         }
     end.to change { Notification.count }.by(2)
   end
+  it 'supports attachments' do
+    stub_request(:post, 'https://api.sendgrid.com/v3/mail/send')
+      .with(
+        body: '{"from":{"email":"app@cinotify.cc"},"subject":"hello w/ attachments","personalizations":[{"to":[{"email":"example@example.com"}]}],"content":[{"type":"text/plain","value":"Please see the attached file(s)"}],"attachments":[{"content":"VGhpcyBpcyBhIGZpbGUgdG8gYmUgYXR0YWNoZWQgdG8gYW4gZW1haWwuCg==","type":"text/plain","filename":"attachment.txt"}]}'
+      )
+      .to_return(status: 200, body: '[]', headers: {})
+    expect do
+      post :notify, params: {
+        to: 'example@example.com',
+        subject: 'hello w/ attachments',
+        body: 'Please see the attached file(s)',
+        attachments: [
+          {
+            content: Base64.strict_encode64(File.read("spec/fixtures/attachment.txt")),
+            type: "text/plain",
+            filename: "attachment.txt"
+          }
+        ]
+      }
+    end.to change { Notification.count }.by(1)
+  end
 end
